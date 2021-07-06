5ac2f5377e67c664c104d3c4857ccd0ecb138962
RobIII <r.janssen@keensystems.eu>
2021-07-06 15:11:39 +0200

    Added IPAddressExtensionMethods

---
+++ IPAddressExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Net;
+
+namespace IPNetworkEFCore
+{
+    public static class IPAddressExtensionMethods
+    {
+        public static string AsHex(this IPAddress ip)
+            => string.Join(string.Empty, ip.GetAddressBytes().Select(b => b.ToString("X2")));
+    }
+}
