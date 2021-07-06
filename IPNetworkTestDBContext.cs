f5798ed9577efae62ae46b548c9b818c1f6c7355
RobIII <r.janssen@keensystems.eu>
2021-07-06 15:15:12 +0200

    Added ClearNetworks and FindNetworksContaining methods.

--- IPNetworkTestDBContext.cs
+++ IPNetworkTestDBContext.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Reflection;
 
 namespace IPNetworkEFCore
@@ -6,6 +9,16 @@ namespace IPNetworkEFCore
     public class IPNetworkTestDBContext : DbContext
     {
         public virtual DbSet<Network> Networks => Set<Network>();
+
+        public void ClearNetworks() => Database.ExecuteSqlRaw($"TRUNCATE TABLE [{nameof(Networks)}]");
+
+        public IEnumerable<Network> FindNetworksContaining(IPAddress ip)
+            => Networks
+                .FromSqlRaw(@$"SELECT * 
+                            FROM [networks]
+                            WHERE 0x{ip.AsHex()} BETWEEN [{nameof(Network.Prefix)}] AND [{nameof(Network.Last)}]
+                            ORDER BY [id]"
+                ).AsEnumerable();
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=IPNetworkTest;Trusted_Connection=True");
 
