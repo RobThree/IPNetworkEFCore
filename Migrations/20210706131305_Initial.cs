e805b4261e2fdb8048f726321e88ece16c1e302f
RobIII <r.janssen@keensystems.eu>
2021-07-06 15:13:26 +0200

    Added initial migration

---
+++ Migrations/20210706131305_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IPNetworkEFCore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Networks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    PrefixLength = table.Column<int>(type: "int", nullable: false),
+                    Prefix = table.Column<byte[]>(type: "varbinary(16)", maxLength: 16, nullable: false),
+                    Last = table.Column<byte[]>(type: "varbinary(16)", maxLength: 16, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Networks", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Networks_Prefix_Last",
+                table: "Networks",
+                columns: new[] { "Prefix", "Last" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Networks");
+        }
+    }
+}
