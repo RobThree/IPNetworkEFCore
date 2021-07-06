cfa397099a76d63ed7db0fe08b787e5b8a1a5a81
RobIII <r.janssen@keensystems.eu>
2021-07-06 15:12:38 +0200

    Added IPNetworkTestDBContext

---
+++ IPNetworkTestDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace IPNetworkEFCore
+{
+    public class IPNetworkTestDBContext : DbContext
+    {
+        public virtual DbSet<Network> Networks => Set<Network>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=IPNetworkTest;Trusted_Connection=True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
