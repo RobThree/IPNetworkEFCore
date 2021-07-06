9a6cc1635b8414ac2c51405111e27048a7f11a95
RobIII <r.janssen@keensystems.eu>
2021-07-06 15:17:41 +0200

    Add test-networks to database.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
 
 namespace IPNetworkEFCore
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var networks = new[] {
+                "192.168.0.0/24",   // IPv4 network
+                "192.168.1.1/32",   // IPv4 network, one IP
+                "2001:0db8:85a3:0000:0000:8a2e:0370:7334/64", //IPv6 network
+                "0:0:0:0:0:FFFF:C0A8:0000/24"       //IPv4 in IPv6 representation network
+            };
+
+            using (var ctx = new IPNetworkTestDBContext())
+            {
+                ctx.ClearNetworks();
+
+                // Add networks
+                foreach (var n in networks)
+                {
+                    if (TryParseNetwork(n, out var network))
+                        ctx.Networks.Add(network with { Description = $"Network: {n}, First: {network.Prefix}, Last: {network.Last}" });
+                }
+                ctx.SaveChanges();
+            }
+        }
+
+        private static bool TryParseNetwork(string value, [NotNullWhen(true)] out Network? result)
+        {
+            var parts = (value ?? string.Empty).Split('/');
+            if (parts.Length == 2 && IPAddress.TryParse(parts[0].Trim(), out var prefix) && int.TryParse(parts[1].Trim(), out var prefixlength))
+            {
+                result = new Network
+                {
+                    Prefix = prefix,
+                    PrefixLength = prefixlength
+                };
+                return true;
+            }
+            result = null;
+            return false;
         }
     }
 }
