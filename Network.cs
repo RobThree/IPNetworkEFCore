b21a1579e8b430cbe3f375d2af2cfce9a36b5320
RobIII <r.janssen@keensystems.eu>
2021-07-06 14:38:23 +0200

    Added Network entity

---
+++ Network.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace IPNetworkEFCore
+{
+    public record Network
+    {
+        public int Id { get; init; }
+
+        [MaxLength(255)]
+        public string? Description { get; init; }
+
+        public IPAddress Prefix { get; init; }
+
+        public int PrefixLength { get; init; }
+    }
+}
